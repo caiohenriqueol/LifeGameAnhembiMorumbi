894a7ac531f870c5e957e4e7caff431d7738dd8f
Shankar Nakai <shankar.nakai@gmail.com>
2017-05-14 19:06:12 -0300

    master Create methods to be used int cell.c

--- cell.h
+++ cell.h
@@ -1,27 +1,67 @@
 #include "utils.h"
 
 //Cell functions
-typedef enum {
+typedef enum e_direction {
 	NORTH  =  1,
 	SOUTH  =  2,
 	EAST   =  3,
 	WEST   =  4
 } direction;
 
-typedef struct cell cell; 
-struct cell {
-	int life;
-	cell *top;
-	cell *bottom;
-	cell *left;
-	cell *right;
-}; 
+/** 
+ * Alias to pointer of int, used to turn de code more verbose
+ */
+typedef int *cell
 
 //FUNCTIONS
-bool isStayAlive(cell c);
-bool isAlive(cell c); 
-bool canRise(cell c);
-int getNeighbors(cell c);
-void Next(cell c, cell *next, direction dir);
-void rise(cell *c);
 
+/** 
+ * Verify if cell can stay alive in this generation  
+ * 
+ * @param cells its a typedef of pointer int 
+ * @param position  its index of array you can check
+ * @return  true to if cell can stay alive, and false if can't stay
+  */
+bool isStayAlive(cell cells, int position);
+
+/** 
+ * Verify if cell is current alive  
+ *
+ * @param cells its a typedef of pointer int 
+ * @param position  its index of array you can check
+ * @return  true to if current alive, and false if can't 
+  */
+bool isAlive(cell c, int position); 
+
+/** 
+ * Verify if cell can turn to alive in next generation  
+ *
+ * @param cells its a typedef of pointer int 
+ * @param position  its index of array you can check
+ * @return  true to if cell can turn to be alive and false if can't 
+  */
+bool canRise(cell c,int position );
+
+
+/** 
+ * Verify if cell can turn to alive in next generation  
+ *
+ * @param cells its a typedef of pointer int 
+ * @param position  its index of array you can check
+ * @return amout of neighbors to this cell
+  */
+int getNeighbors(cell c);
+
+/** 
+ * Kill all cell in this array 
+ *
+ * @param cells its a typedef of pointer int 
+  */
+void kill(cell *c);
+
+/** 
+ * Turn to alive all cell in this array 
+ *
+ * @param cells its a typedef of pointer int 
+  */
+void rise(cell *c);
